1c7995b419bb7ce656ddf2f013afbc176559b629
Rudolf Achter <rudolf.achter@gmail.com>
2022-08-14 21:49:07 +0200

    update: test searching did

---
+++ src/test_db_items_by_did_owner.py
@@ -0,0 +1,26 @@
+import pymongo
+from pprint import pprint
+import config.secrets
+import json
+from chia.util.bech32m import decode_puzzle_hash, encode_puzzle_hash
+
+# Your Internet IP must be added to MongoDB Atlas Network Access IPs
+
+myuser=config.secrets.dbuser
+mypassword=config.secrets.dbpass
+
+dbclient = pymongo.MongoClient("mongodb+srv://"+myuser+":"+mypassword+"@chia-stuff.g7ivxgh.mongodb.net/?retryWrites=true&w=majority")
+
+db = dbclient.chiania
+col = db.items
+
+#"0x3cf536f516dadd52dcead0eb4e3d80501d3936a61d9bb6ab76507c5d41b10b42"
+decoded_did=decode_puzzle_hash("did:chia:18n6ndagkmtw49h826r45u0vq2qwnjd4xrkdmd2mk2p796sd3pdpqtj4h86")
+search_did='0x' + decoded_did.hex()
+
+print(search_did)
+
+items=col.find({"owner_did": search_did})
+
+for dat in items:
+    pprint(dat,indent=2)
